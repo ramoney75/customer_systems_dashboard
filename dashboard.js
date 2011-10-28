701c6e432ed9d3ed6e4c8f26326b87e033938950
Bertrand <translated@gmail.com>
2011-10-28 11:15:25 +1100

    externalised the js

---
+++ dashboard.js
@@ -0,0 +1,22 @@
+function renderJson(parsedJson) {
+  var pipelineTemplate = haml.compileHaml('pipeline-template');
+  for (i = 0; i < parsedJson.length; i++) {
+    pipeline = parsedJson[i];
+    $('#formatted-data').html(pipelineTemplate.call(this, {pipeline: pipeline}));
+  }
+}
+
+function processData(data) {
+  $('#raw-json').append(data);
+  var parsedJson = $.parseJSON(data);
+  $('formatted-data').append(parsedJson[0]['name']);
+  renderJson(parsedJson);
+}
+
+function fetchData() {
+  $.get('http://10.113.192.74/pipelines', processData);
+}
+
+$(document).ready(function () {
+  fetchData();
+});
